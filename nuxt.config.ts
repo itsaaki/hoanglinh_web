35abd2e359418b0aeae773fcf737195482ba4816
minhNN03 <minhnnguyen003@gmail.com>
2026-01-02 22:27:54 +0700

    fix: Change Template

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  modules: [
+    '@nuxt/content',
+  ],
+  devtools: { enabled: true },
+  compatibilityDate: '2024-04-03',
 })
