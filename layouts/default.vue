4b5826207cc042e67ae3eca092e1b499df9ed50a
Duy Anh Do <139994945+itsaaki@users.noreply.github.com>
2026-01-11 11:13:55 +0700

    Add files via upload

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="min-h-screen flex flex-col">
+    <AppHeader />
+
+    <main class="flex-grow">
+      <slot />
+    </main>
+
+    <footer class="bg-blue-900 text-white p-8 text-center mt-auto">
+      &copy; 2026 Công ty TNHH Hoàng Linh
+    </footer>
+  </div>
+</template>
